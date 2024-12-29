8812ca9af6de1ee4d5a41731d59983f4a332fc99
nikeshcohen <nikeshcohen@outlook.com>
2024-12-29 18:30:40 +0200

    add marketing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,111 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
 
-function page() {
+import HeroImage from "@/public/hero.png";
+import Logo from "@/public/logo.png";
+
+import { RainbowButton } from "@/components/ui/rainbow-button";
+
+export default function Home() {
   return (
-    <div>page</div>
-  )
-}
+    <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      \{" "}
+      <div className="flex items-center justify-between py-5">
+        <Link href="/" className="flex items-center gap-2">
+          <Image src={Logo} alt="Logo" className="size-10" />
+          <h3 className="text-3xl font-semibold">
+            Invo<span className="text-blue-500">Snap</span>
+          </h3>
+        </Link>
+        <Link href="/login">
+          <RainbowButton>Get Started</RainbowButton>
+        </Link>
+      </div>
+      <section className="relative flex flex-col items-center justify-center py-12 lg:py-20">
+        <div className="text-center">
+          <span className="rounded-full bg-primary/10 px-4 py-2 text-sm font-medium tracking-tight text-primary">
+            Introducing InvoSnap 1.0
+          </span>
+          <h1 className="mt-8 text-4xl font-semibold tracking-tighter sm:text-6xl md:text-7xl lg:text-8xl">
+            Invoicing made{" "}
+            <span className="-mt-2 block bg-gradient-to-l from-blue-500 via-teal-500 to-green-500 bg-clip-text text-transparent">
+              super easy!
+            </span>
+          </h1>
 
-export default page
+          <p className="mx-auto mt-4 max-w-xl text-muted-foreground lg:text-lg">
+            Creating Invoices can be a pain! We at InvoSnap make it super easy
+            for you to get paid in time!
+          </p>
+
+          <div className="mb-12 mt-7">
+            <Link href="/login">
+              <RainbowButton>Get Unlimted Access</RainbowButton>
+            </Link>
+          </div>
+        </div>
+
+        <div className="relative mx-auto mt-12 w-full items-center py-12">
+          <svg
+            className="absolute inset-0 -mt-24 blur-3xl"
+            style={{ zIndex: -1 }}
+            fill="none"
+            viewBox="0 0 400 400"
+            height="100%"
+            width="100%"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clipPath="url(#clip0_10_20)">
+              <g filter="url(#filter0_f_10_20)">
+                <path
+                  d="M128.6 0H0V322.2L106.2 134.75L128.6 0Z"
+                  fill="#03FFE0"
+                ></path>
+                <path
+                  d="M0 322.2V400H240H320L106.2 134.75L0 322.2Z"
+                  fill="#7C87F8"
+                ></path>
+                <path
+                  d="M320 400H400V78.75L106.2 134.75L320 400Z"
+                  fill="#4C65E4"
+                ></path>
+                <path
+                  d="M400 0H128.6L106.2 134.75L400 78.75V0Z"
+                  fill="#043AFF"
+                ></path>
+              </g>
+            </g>
+            <defs>
+              <filter
+                colorInterpolationFilters="sRGB"
+                filterUnits="userSpaceOnUse"
+                height="720.666"
+                id="filter0_f_10_20"
+                width="720.666"
+                x="-160.333"
+                y="-160.333"
+              >
+                <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
+                <feBlend
+                  in="SourceGraphic"
+                  in2="BackgroundImageFix"
+                  mode="normal"
+                  result="shape"
+                ></feBlend>
+                <feGaussianBlur
+                  result="effect1_foregroundBlur_10_20"
+                  stdDeviation="80.1666"
+                ></feGaussianBlur>
+              </filter>
+            </defs>
+          </svg>
+          <Image
+            src={HeroImage}
+            alt="Hero image"
+            className="relative w-full rounded-lg border object-cover shadow-2xl lg:rounded-2xl"
+          />
+        </div>
+      </section>
+    </main>
+  );
+}
